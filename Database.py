5c6615b3fa5b53dd93c227fa0dd0ea4ac26926ae
IanLampic <lampic.ian@gmail.com>
2023-05-02 11:55:14 +0200

    Database

--- Database.py
+++ Database.py
@@ -1,15 +1,13 @@
 import pandas as pd
 
-<<<<<<< HEAD
 podatki_characters = pd.read_csv(r'podatki/Characters.csv', sep=";")
 df = pd.DataFrame(podatki_characters)
 
-print(df)
-=======
-podatki_ch = pd.read_csv ("C:/Users/mo6404/Desktop/Projekt_baze/Svecano-prisegam-da-imam-nekaj-za-bregom/podatki/Characters.csv", sep=";")
-df = pd.DataFrame(podatki_ch)
-
-
-
-print(df)
->>>>>>> 7e56db0bc3881f23c2393292994a6976bed8a197
+students = df.loc[df['Job'] == "Student"]
+professors = df.loc[df['Job'] == r'.*Professor.*']
+#for i in range(len(df)):
+#    if df["Job"] == "Student":df.loc[df['Job'] == "Student"]
+#        students = df
+        #df['Job'] = df['Job'].replace(['Headmaster', 'Keeper of Keys and Grounds | Professor'], 'Professor')
+print(students)
+print(professors)
