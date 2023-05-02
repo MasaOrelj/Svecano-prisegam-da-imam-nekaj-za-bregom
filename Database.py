4e29c445792ca457d5e901815e8d32b2dfb82cb8
IanLampic <lampic.ian@gmail.com>
2023-05-02 12:01:00 +0200

    Proba

--- Database.py
+++ Database.py
@@ -5,6 +5,7 @@ df = pd.DataFrame(podatki_characters)
 
 students = df.loc[df['Job'] == "Student"]
 professors = df.loc[df['Job'] == r'.*Professor.*']
+
 #for i in range(len(df)):
 #    if df["Job"] == "Student":df.loc[df['Job'] == "Student"]
 #        students = df
