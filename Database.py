5a45cf860db8a977a3d574631b40d7ed681c9be4
Pavla Novak <pavlanovak2001@gmail.com>
2023-05-02 11:51:52 +0200

    proba

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-podatki_ch = pd.read_csv ("C:/Users/mo6404/Desktop/Projekt_baze/Svecano-prisegam-da-imam-nekaj-za-bregom/podatki/Characters.csv", sep=";")
+podatki_ch = pd.read_csv ("podatki/Characters.csv", sep=";")
 df = pd.DataFrame(podatki_ch)
 
 
