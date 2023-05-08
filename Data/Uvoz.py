84f2fd422a76eb981d0b209aeaf8a4e343185b3e
MasaOrelj <masa.orelj2@gmail.com>
2023-05-08 18:32:05 +0200

    popravek tabel

--- Data/Uvoz.py
+++ Data/Uvoz.py
@@ -31,4 +31,4 @@ def uvozi_v_sql(df, ime):
     repo.df_to_sql_create(df, ime, add_serial=True, use_camel_case=True)
     repo.df_to_sql_insert(df, ime, use_camel_case=True)
 
-uvozi_v_sql(professors2, "professors")
+uvozi_v_sql(professors2, "professor")
