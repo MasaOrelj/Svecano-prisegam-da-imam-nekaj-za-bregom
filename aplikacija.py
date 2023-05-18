0f24b367c97f1d2d6a69939a2df529364616871e
IanLampic <lampic.ian@gmail.com>
2023-05-18 13:42:42 +0200

    prijava

--- aplikacija.py
+++ aplikacija.py
@@ -2,13 +2,16 @@
 # -*- encoding: utf-8 -*-
 
 # uvozimo bottle.py
-from bottleext import get, post, run, request, template, redirect, static_file, url, response, template_user, debug
-import bottle
+from bottleext import *
 
 from Data.Database import Repo
 from Data.Modeli import *
 from Data.Services import AuthService
 from functools import wraps
+
+import Data.auth as auth_public
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
 import os
 
@@ -19,91 +22,47 @@ SERVER_PORT = os.environ.get('BOTTLE_PORT', 8081)
 RELOADER = os.environ.get('BOTTLE_RELOADER', True)
 DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
-@bottle.get('/prijava/')
-def prijava_get():
-    return template_user('prijava.html', napaka=None)
-
-@bottle.post('/prijava/')
-def prijava_post():
-    #uporabnisko_ime = bottle.request.forms.getunicode('uporabnisko_ime')
-    #geslo_v_cistopisu = bottle.request.forms.getunicode('geslo')
-    #if not uporabnisko_ime:
-    #    return bottle.template('registracija.html', napaka="Vnesi uporabniško ime!")
-    #try:  
-        #Uporabnik.prijava(uporabnisko_ime, geslo_v_cistopisu)
-        #bottle.response.set_cookie(PISKOTEK_UPORABNISKO_IME, uporabnisko_ime, path="/", secret=SKRIVNOST)
-        bottle.redirect('/')
-    #except ValueError as e:
-    #    return bottle.template("prijava.html", napaka=e.args[0])
-
-@bottle.post('/odjava/')
-def odjava():
-    #bottle.response.delete_cookie(PISKOTEK_UPORABNISKO_IME, path='/')
-    bottle.redirect('/')
-
-@bottle.get("/registracija/")
-def registracija_get():
-    return bottle.template("registracija.html", napaka=None)
-
-@bottle.post("/registracija/")
-def registracija_post():
-    #uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
-    #geslo_v_cistopisu = bottle.request.forms.getunicode("geslo")
-    #if not uporabnisko_ime:
-    #    return bottle.template("registracija.html", napaka="Vnesi uporabniško ime!")
-    #try:
-    #    Uporabnik.registracija(uporabnisko_ime, geslo_v_cistopisu)
-    #    bottle.response.set_cookie(
-    #        PISKOTEK_UPORABNISKO_IME, uporabnisko_ime, path="/", secret=SKRIVNOST
-    #    )
-        bottle.redirect("/")
-    #except ValueError as e:
-    #    return bottle.template(
-    #        "registracija.html", napaka=e.args[0]
-    #    )
-
-@bottle.get('/')
+@get('/')
 def osnovna_stran():
     return bottle.template('osnovna_stran.html')
 
-@bottle.get('/profile/')
+@get('/profile')
 def profile_get():
-    return bottle.template("profile.html") 
+    return template("profile.html") 
 
-@bottle.post('/profile/')
+@post('/profile')
 def profile_post():
-    bottle.redirect('/') 
+    redirect('/') 
  
-@bottle.get('/house/')
+@get('/house')
 def houses_get():
-    return bottle.template("house.html") 
+    return template("house.html") 
 
-@bottle.post('/house/')
+@post('/house')
 def houses_post():
-    bottle.redirect('/')   
+    redirect('/')   
 
-
-@bottle.get('/subjects/')
+@get('/subjects')
 def subjects_get():
-    return bottle.template("subjects.html") 
+    return template("subjects.html") 
 
-@bottle.post('/subjects/')
+@post('/subjects')
 def subjects_post():
-    bottle.redirect('/')   
+    redirect('/')   
 
-@bottle.get('/forum/')
+@get('/forum')
 def forum_get():
-    return bottle.template("forum.html") 
+    return template("forum.html") 
 
-@bottle.post('/forum/')
+@post('/forum')
 def forum_post():
-    bottle.redirect('/')
+    redirect('/')
 
-@get('/prijava/') 
+@get('/prijava') 
 def prijava_get():
     return template("prijava.html")
 
-@post('/prijava/') 
+@post('/prijava') 
 def prijava_post():
     uporabnisko_ime = request.forms.get('uporabnisko_ime')
     geslo = request.forms.get('geslo')
@@ -121,15 +80,22 @@ def prijava_post():
         redirect(url('prijava_get'))
         return
     if geslo != hashBaza:
-         #nastaviSporocilo('Nekaj je šlo narobe.') 
          redirect(url('prijava_get'))
          return
-#    redirect(url('pregled_rezervacij_gosta'))
     redirect(url('osnovna_stran', id_gosta=id_gosta))
+
+@get('/odjava')
+def odjava():
+    response.delete_cookie("uporabnik")
+    response.delete_cookie("rola")
+    return template('osnova_stran.html', napaka=None)
+
+
+conn = psycopg2.connect(database=auth_public.db, host=auth_public.host, user=auth_public.user, password=auth_public.password, port=DB_PORT)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
 debug(True)
 
- 
 # poženemo strežnik na podanih vratih, npr. http://localhost:8080/
 if __name__ == "__main__":
     run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
